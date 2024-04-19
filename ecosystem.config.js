3093ff4440ae07b653addbcb4536e35bd350b713
taqqr <crazymingzai@gmail.com>
2024-04-19 15:38:12 +0800

    change config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
     args: "start",
     env_production: {
       NODE_ENV: "production",
-      PORT: 7202,
-      REDIS_URL: "redis://:password@redis_db:6379"
+      PORT: 7203,
+      REDIS_URL: process.env.REDIS_URL,// "redis://:password@redis_db:6379"
     },
     env_development: {
       NODE_ENV: "development"
